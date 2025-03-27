c5766603de882831542984862fc52ee30b19e4a8
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-03-27 09:56:59 +0100

    B2N2

--- fib_KL4/Program.cs
+++ fib_KL4/Program.cs
@@ -2,23 +2,23 @@
 {
     internal class Program
     {
-
         public static void Fibonacci(int n)
         {
             
             int i = 0;
             Int128 max,zmienna_tmp;
-            Int128.TryParse("170141183460469231731687303715884105727",out max);
-
-            Console.WriteLine("Max: " + max);
-
             Int128[] fib = new Int128[n];
             char[] liczba_binarna = new char[128];
 
+            Int128.TryParse("170141183460469231731687303715884105727",out max);
+            Console.WriteLine("Max: " + max);
+        
             fib[0] = 0;
             fib[1] = 1;
+
             Console.WriteLine(fib[0] + " ");
             Console.WriteLine(fib[1] + " ");
+
             for (i = 2; i < n; i++)
             {
 
@@ -33,37 +33,23 @@
                     zmienna_tmp >>= 1;
                 }
 
-
-
-                // if (fib[i] > max && fib[i - 1] < fib[i] && fib[i] > 0)
-                // {
-
-              if (liczba_binarna[0] == '1')
-              {
+                if (liczba_binarna[0] == '1')
+                {
                     break;
                 }
 
                 zmienna_tmp = fib[i];
-                    zmienna_tmp.ToString();
+                zmienna_tmp.ToString();
 
-                    Console.WriteLine("Max reached at index: " + i);
-                    Console.WriteLine(liczba_binarna);
-                    // break;
-                    // }
-                    Console.WriteLine(i + " " + fib[i] + " ");
-
-              
+                Console.WriteLine("Max reached at index: " + i);
+                Console.WriteLine(liczba_binarna);
+                Console.WriteLine(i + " " + fib[i] + " ");
             }
-
-
-
         }
         static void Main(string[] args)
         {
-            //170 141 183 460 469 231 731 687 303 715 884 105 727
             Fibonacci(200);
 
-            Console.WriteLine("Hello World!");
         }
     }
 }
